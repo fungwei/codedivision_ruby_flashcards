37f3e6b874697ec94ccb5190f98c6629594be6ff
Fung Wei Tan Tai <fungwei@goget.my>
2014-10-13 10:38:00 +0800

    base classes without game code

---
+++ flashcards.rb
@@ -0,0 +1,79 @@
+#Flashcard class
+class Flashcard
+	attr_reader :definition, :description
+	# attr_writer :complete
+	def initialize(definition, term)
+		@definition = definition
+		@term = term
+		# @complete = false
+	end
+
+	# def complete?
+	# 	@complete
+	# end
+
+	# def complete!
+	# 	@complete = true
+	# end
+end
+
+class FlashcardStack
+	def initialize
+		@stack = []
+		@finish = false
+	end
+
+	def add_flashcard(flashcard)
+		@stack << flashcard
+	end
+
+	def add_file(file)
+		definition = ""
+		term = ""
+		File.open(file) do |f|
+			line_count = 0
+		  f.each_line do |line|
+		  	# puts line_count
+			case line_count % 3
+			when 0
+				definition = line
+				# puts definition
+			when 1
+				term = line
+				# puts term
+				flashcard = Flashcard.new(definition, term)
+				add_flashcard(flashcard)
+				# puts flashcard.complete?
+			end
+				
+
+
+			line_count += 1
+		  end
+		  # puts @stack.inspect
+		end
+	end
+
+	def get_random_card
+		@stack.sample
+		# tempflashcard.complete!
+	end
+
+	def remove_card(flashcard)
+		@stack.delte(flashcard)
+	end
+
+end
+
+
+flashcard_stack = FlashcardStack.new
+
+flashcard_stack.add_file("flashcard_samples.txt")
+
+flashcard = flashcard_stack.get_random_card
+
+puts flashcard.definition
+puts flashcard.term
+
+
+
